6b23e50f0f518e7ea0e87950db4e5c4fc78d650f
sahdan17 <sahdan.wira.17@gmail.com>
2025-07-10 08:39:55 +0700

    up

--- routes/frontend/data.controller.js
+++ routes/frontend/data.controller.js
@@ -2,9 +2,14 @@ import express from 'express'
 import {
     getAllData
 } from '../../controllers/frontend/data.controller.js'
+import {
+    verifyTokenSA,
+    verifyTokenAdmin,
+    verifyTokenUser
+} from '../../middlewares/user.middleware.js'
 
 const router = express.Router()
 
-router.post('/getAllData', getAllData)
+router.post('/getAllData', verifyTokenUser, getAllData)
 
 export default router
