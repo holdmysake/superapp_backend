6cb46360fcdffe7c182a9c644f6eda1b01c3234b
sahdan17 <sahdan.wira.17@gmail.com>
2025-07-16 11:59:20 +0700

    up

--- controllers/panel/data.controller.js
+++ controllers/panel/data.controller.js
@@ -1,112 +1,10 @@
 import { Parser } from 'json2csv'
+import ExcelJS from 'exceljs'
+import archiver from 'archiver'
 import { Op } from "sequelize"
 import defineUserDataModel from "../../models/pressure.model.js"
 import moment from "moment-timezone"
 import Spot from '../../models/spot.model.js';
-
-// export const downloadDataCSV = async (req, res) => {
-//     try {
-//         const { field_id, spot_id, timestamp } = req.body
-
-//         const tableName = `pressure_${field_id}`
-//         const Pressure = defineUserDataModel(tableName)
-
-//         const startOfDay = moment.tz(timestamp, 'YYYY-MM-DD')
-//                             .startOf('day')
-//                             .toDate()
-//         const endOfDay   = moment(startOfDay)
-//                             .add(1, 'day')
-//                             .toDate()
-
-//         const pressureData = await Pressure.findAll({
-//             where: {
-//                 spot_id,
-//                 timestamp: {
-//                     [Op.gte]: startOfDay,
-//                     [Op.lt]: endOfDay
-//                 }
-//             },
-//             attributes: ['timestamp','psi'],
-//             order: [['timestamp','ASC']]
-//         })
-
-//         const csvData = pressureData.map(entry => {
-//             const ts = moment(entry.timestamp)
-//             return {
-//                 date: ts.format('YYYY-MM-DD'),
-//                 time: ts.format('HH-mm-ss'),
-//                 psi: entry.psi
-//             }
-//         })
-
-//         const fields = ['field_id', 'date', 'time', 'psi']
-//         const parser = new Parser({ fields })
-//         const csv = parser.parse(csvData)
-
-//         const fileName = `pressure_${field_id}_${moment(timestamp).format('DD_MM_YYYY')}.csv`
-        
-//         res.header('Content-Type', 'text/csv')
-//         res.attachment(fileName)
-//         res.send(csv)
-//     } catch (error) {
-//         console.error(error)
-//         res.status(500).json({ message: error.message })
-//     }
-// }
-
-// export const downloadDataCSV = async (req, res) => {
-//     try {
-//         const { field_id, tline_id, timestamp } = req.body
-
-//         const tableName = `pressure_${field_id}`
-//         const Pressure = defineUserDataModel(tableName)
-
-//         const startOfDay = moment.tz(timestamp, 'YYYY-MM-DD').startOf('day').toDate()
-//         const endOfDay = moment(startOfDay).add(1, 'day').toDate()
-
-//         const spots = await Spot.findAll({
-//             where: { tline_id },
-//             attributes: ['spot_id']
-//         })
-
-//         const spotIds = spots.map(spot => spot.spot_id)
-
-//         const pressureData = await Pressure.findAll({
-//             where: {
-//                 spot_id: spotIds,
-//                 timestamp: {
-//                     [Op.gte]: startOfDay,
-//                     [Op.lt]: endOfDay
-//                 }
-//             },
-//             attributes: ['spot_id', 'timestamp', 'psi'],
-//             order: [['timestamp', 'ASC']]
-//         })
-
-//         const csvData = pressureData.map(entry => {
-//             const ts = moment(entry.timestamp)
-//             return {
-//                 spot_id: entry.spot_id,
-//                 date: ts.format('YYYY-MM-DD'),
-//                 time: ts.format('HH-mm-ss'),
-//                 psi: entry.psi
-//             }
-//         })
-
-//         const fields = ['spot_id', 'date', 'time', 'psi']
-//         const parser = new Parser({ fields })
-//         const csv = parser.parse(csvData)
-
-//         const fileName = `pressure_${field_id}_${moment(timestamp).format('DD_MM_YYYY')}.csv`
-
-//         res.header('Content-Type', 'text/csv')
-//         res.attachment(fileName)
-//         res.send(csv)
-//     } catch (error) {
-//         console.error(error)
-//         res.status(500).json({ message: error.message })
-//     }
-// }
 
 export const downloadDataCSV = async (req, res) => {
     try {
@@ -156,16 +54,32 @@ export const downloadDataCSV = async (req, res) => {
         }
 
         const sortedSpotIds = Array.from(spotSet).sort((a, b) => a - b)
-        const fields = ['date', 'time', ...sortedSpotIds]
+        const headers = ['date', 'time', ...sortedSpotIds]
+        const rows = Array.from(dataMap.values())
 
-        const finalRows = Array.from(dataMap.values())
-        const parser = new Parser({ fields })
-        const csv = parser.parse(finalRows)
+        const datePart = moment(timestamp).format('DD_MM_YYYY')
+        const baseFileName = `pressure_${tline_id}_${datePart}`
 
-        const fileName = `pressure_${tline_id}_${moment(timestamp).format('DD_MM_YYYY')}.csv`
-        res.header('Content-Type', 'text/csv')
-        res.attachment(fileName)
-        res.send(csv)
+        const parser = new Parser({ fields: headers })
+        const csv = parser.parse(rows)
+
+        const workbook = new ExcelJS.Workbook()
+        const worksheet = workbook.addWorksheet('Pressure Data')
+        worksheet.columns = headers.map(h => ({ header: h, key: h }))
+        rows.forEach(row => worksheet.addRow(row))
+        const excelBuffer = await workbook.xlsx.writeBuffer()
+
+        const zipName = `${baseFileName}.zip`
+        res.setHeader('Content-Type', 'application/zip')
+        res.setHeader('Content-Disposition', `attachment; filename=${zipName}`)
+
+        const archive = archiver('zip')
+        archive.pipe(res)
+
+        archive.append(csv, { name: `${baseFileName}.csv` })
+        archive.append(excelBuffer, { name: `${baseFileName}.xlsx` })
+        
+        await archive.finalize()
     } catch (error) {
         console.error(error)
         res.status(500).json({ message: error.message })
