560d1658f0cd26eed15fd0016f18257bd7341a19
sahdan17 <sahdan.wira.17@gmail.com>
2025-07-16 09:37:17 +0700

    up

--- controllers/panel/data.controller.js
+++ controllers/panel/data.controller.js
@@ -1,3 +1,4 @@
+import { Parser } from 'json2csv'
 import { Op } from "sequelize"
 import defineUserDataModel from "../../models/pressure.model.js"
 import moment from "moment-timezone"
@@ -28,19 +29,25 @@ export const downloadDataCSV = async (req, res) => {
             order: [['timestamp','ASC']]
         })
 
-        const formattedData = {
-            field_id: field_id,
-            pressures: pressureData.map(entry => {
-                const ts = moment(entry.timestamp)
-                return {
-                    date: ts.format('YYYY-MM-DD'),
-                    time: ts.format('HH-mm-ss'),
-                    psi: entry.psi
-                }
-            })
-        }
+        const csvData = pressureData.map(entry => {
+            const ts = moment(entry.timestamp)
+            return {
+                field_id: field_id,
+                date: ts.format('YYYY-MM-DD'),
+                time: ts.format('HH-mm-ss'),
+                psi: entry.psi
+            }
+        })
 
-        res.json(formattedData)
+        const fields = ['field_id', 'date', 'time', 'psi']
+        const parser = new Parser({ fields })
+        const csv = parser.parse(csvData)
+
+        const fileName = `pressure_${field_id}_${moment(timestamp).format('YYYYMMDD')}.csv`
+        
+        res.header('Content-Type', 'text/csv')
+        res.attachment(fileName)
+        res.send(csv)
     } catch (error) {
         console.error(error)
         res.status(500).json({ message: error.message })
