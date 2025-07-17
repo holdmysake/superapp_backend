a363e8a88d748bb5e7c95300f2c2dc5a1ac31dc0
sahdan17 <sahdan.wira.17@gmail.com>
2025-07-17 22:03:19 +0700

    Create wa_login.js

---
+++ models/wa_login.js
@@ -0,0 +1,48 @@
+import { DataTypes } from 'sequelize'
+import sequelize from '../config/db.js'
+import Field from './field.model.js'
+import Spot from './spot.model.js'
+
+const WALogin = sequelize.define('wa_login', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    field_id: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+        references: {
+            model: Field,
+            key: 'field_id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+    },
+    spot_id: {
+        type: DataTypes.STRING(10),
+        allowNull: true,
+        references: {
+            model: Spot,
+            key: 'spot_id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+    },
+    is_login: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
+    },
+    no_wa: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    }
+}, {
+    tableName: 'wa_login',
+    timestamps: false,
+    underscored: true
+})
+
+export default WALogin
